272cecac900394b438856ef5b164d136e4884045
Linus Lindblad <linus@lindblad.net>
2022-12-03 14:11:06 +0100

    more attempts

--- prometheus/configmap/check-promconf.sh
+++ prometheus/configmap/check-promconf.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # $1 arg = File to be checked
-# $2 arg = url to post
+# $2 arg = File to store output in
+# $3 arg = url to post
 
 ARGFILE=$(md5sum "$1")
-COMPAREFILE=$(cat "$1.md5")
+COMPAREFILE=$(cat "$2")
 
 if [ "$ARGFILE" = "$COMPAREFILE" ]
 then
   sleep 1
 else
   echo "File diff found, triggering prometheus reload"
-  md5sum "$1" > "$1.md5"
-  curl --insecure -X POST "$2"
+  md5sum "$1" > "$2"
+  curl --insecure -X POST "$3"
 fi
